2bee600ef9b441d7808ddc4576827b93ca121c0a
June <june.elexpuru@alumni.mondragon.edu>
2024-05-10 13:03:16 +0200

    quinto

--- Verde Claro.R
+++ Verde Claro.R
@@ -1,12 +1,6 @@
 print("Jon Ayala, Las bananas contienen potasio, y un isotopo de este elemento, el potasio-40, es ligeramente radioactivo")
 print("Sara Ju Ansorena Esnal Miner")
 print("Libe Arana, Jupiter contiene 79 lunas eh")
-<<<<<<< HEAD
-
-=======
 print("Maider Torres, Donde esta???")
-<<<<<<< HEAD
->>>>>>> 61113d7b776c07c0e64995f6698704182fab53b3
-=======
 print("Anne Martin")
->>>>>>> efa9611a395003be9dde2856498c985ee12f981f
+print("June Elexpuru")
