efa9611a395003be9dde2856498c985ee12f981f
Anne Martin <anne.martinb@alumni.mondragon.edu>
2024-05-10 12:56:49 +0200

    cuarto cambio

--- Verde Claro.R
+++ Verde Claro.R
@@ -1,3 +1,4 @@
 print("Jon Ayala, Las bananas contienen potasio, y un isotopo de este elemento, el potasio-40, es ligeramente radioactivo")
 print("Libe Arana, Jupiter contiene 79 lunas eh")
 print("Maider Torres, Donde esta???")
+print("Anne Martin")
