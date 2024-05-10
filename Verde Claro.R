df32782d385b6930f77545493b8759c13cbb9f4b
Libe Arana <libe.arana@alumni.mondragon.edu>
2024-05-10 12:49:02 +0200

    segundo

--- Verde Claro.R
+++ Verde Claro.R
@@ -1,2 +1,2 @@
 print("Jon Ayala, Las bananas contienen potasio, y un isotopo de este elemento, el potasio-40, es ligeramente radioactivo")
-print("Libe Arana, Jupiter contiene 79 lunas")
+print("Libe Arana, Jupiter contiene 79 lunas eh")
