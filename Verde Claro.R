8d5d8814a2fafc4aa1ecc8ca901b27f9d1ec3058
Maider Torres <maider.torres@alumni.mondragon.edu>
2024-05-10 12:53:14 +0200

    tercer cambio

--- Verde Claro.R
+++ Verde Claro.R
@@ -1,3 +1,3 @@
 print("Jon Ayala, Las bananas contienen potasio, y un isotopo de este elemento, el potasio-40, es ligeramente radioactivo")
 print("Libe Arana, Jupiter contiene 79 lunas eh")
-print("Maider Torres, Donde esta??")
+print("Maider Torres, Donde esta???")
